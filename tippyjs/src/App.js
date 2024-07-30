a68bcbb131b463699d8a37ced02a6b2a59411a92
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2024-07-30 10:51:17 +0530

    :+1:

--- tippyjs/src/App.js
+++ tippyjs/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Tippy js
         </a>
       </header>
     </div>
